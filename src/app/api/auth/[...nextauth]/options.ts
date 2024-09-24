7a27d299c1b4ce641f8c9fe23696e7276e5854e7
jlauza <lauzajayson@gmail.com>
2024-09-24 18:57:34 +0800

    disable cause of error

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -20,32 +20,33 @@ export const options: NextAuthOptions = {
         },
       },
     }),
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        username: { label: "Username", type: "text", placeholder: "Username" },
-        password: {
-          label: "Password",
-          type: "password",
-          placeholder: "Password",
-        },
-      },
-      async authorize(credentials) {
-        // pull user data from database
+    // TEMPORARILY DISABLE ===========================>
+    // CredentialsProvider({
+    //   name: "Credentials",
+    //   credentials: {
+    //     username: { label: "Username", type: "text", placeholder: "Username" },
+    //     password: {
+    //       label: "Password",
+    //       type: "password",
+    //       placeholder: "Password",
+    //     },
+    //   },
+    //   async authorize(credentials) {
+    //     // pull user data from database
 
-        // This is test only
-        // Replace this with actual data pull from DATABASE
-        const user = { id: 1, name: "Test User", password: "123456" };
+    //     // This is test only
+    //     // Replace this with actual data pull from DATABASE
+    //     const user = { id: 1, name: "Test User", password: "123456" };
 
-        if (
-          credentials.username === user.name &&
-          credentials.password === user.password
-        ) {
-          return user;
-        } else {
-          return null;
-        }
-      },
-    }),
+    //     if (
+    //       credentials.username === user.name &&
+    //       credentials.password === user.password
+    //     ) {
+    //       return user;
+    //     } else {
+    //       return null;
+    //     }
+    //   },
+    // }),
   ],
 };
