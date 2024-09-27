5ce423c388f98cd37e71f4b51249ee846abb265d
jlauza <lauzajayson@gmail.com>
2024-09-27 16:10:11 +0800

    revert

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -20,33 +20,32 @@ export const options: NextAuthOptions = {
         },
       },
     }),
-    // TEMPORARILY DISABLE ===========================>
-    // CredentialsProvider({
-    //   name: "Credentials",
-    //   credentials: {
-    //     username: { label: "Username", type: "text", placeholder: "Username" },
-    //     password: {
-    //       label: "Password",
-    //       type: "password",
-    //       placeholder: "Password",
-    //     },
-    //   },
-    //   async authorize(credentials) {
-    //     // pull user data from database
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "Username" },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "Password",
+        },
+      },
+      async authorize(credentials) {
+        // pull user data from database
 
-    //     // This is test only
-    //     // Replace this with actual data pull from DATABASE
-    //     const user = { id: 1, name: "Test User", password: "123456" };
+        // This is test only
+        // Replace this with actual data pull from DATABASE
+        const user = { id: 1, name: "Test User", password: "123456" };
 
-    //     if (
-    //       credentials.username === user.name &&
-    //       credentials.password === user.password
-    //     ) {
-    //       return user;
-    //     } else {
-    //       return null;
-    //     }
-    //   },
-    // }),
+        if (
+          credentials.username === user.name &&
+          credentials.password === user.password
+        ) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
   ],
 };
